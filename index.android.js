839fae4b43ff8743e6acf161a816aad22899ccf3
s-kobets <s-kobets617@yandex.ru>
2017-07-25 09:34:08 +0300

    anroid run success

--- index.android.js
+++ index.android.js
@@ -9,23 +9,17 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
 } from 'react-native';
+import Map from './components/map';
 
 export default class mymaps extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        {/*<Image style={styles.marker} source={require('./src/image/mumimaps_placemark.svg')} />*/}
+        <Map />
       </View>
     );
   }
@@ -37,17 +31,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('mymaps', () => mymaps);
